e63faa626a1f344575dac99223df251f36230965
lucas <lucasbomfimsobral@gmail.com>
2024-05-18 13:59:40 -0300

    Creating a new task role

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { Pool } = require('pg');
-const cors = require('cors'); // Importe o pacote cors
+const cors = require('cors'); 
 require('dotenv').config();
 
 const app = express();
 
 // Use o middleware CORS
 app.use(cors());
+app.use(express.json()); // Middleware para analisar o corpo das solicitações como JSON
 
 // Configuração do pool de conexão com o banco de dados
 const pool = new Pool({
@@ -31,6 +32,27 @@ app.get('/tasks', async (req, res) => {
     } catch (err) {
         console.error('Erro ao buscar dados do banco de dados:', err);
         res.status(500).json({ error: 'Erro ao buscar dados do banco de dados' });
+    }
+});
+
+// Rota para adicionar uma nova tarefa ao banco de dados
+app.post('/tasks', async (req, res) => {
+    try {
+        const { task } = req.body;
+
+        // Verifique se o campo de tarefa foi fornecido
+        if (!task) {
+            return res.status(400).json({ error: 'O campo de tarefa é obrigatório' });
+        }
+
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO tasks (task, created_at) VALUES ($1, NOW()) RETURNING *', [task]);
+        const newTask = result.rows[0];
+        client.release();
+        res.status(201).json(newTask); // Retorna a nova tarefa com status 201 (Created)
+    } catch (err) {
+        console.error('Erro ao adicionar tarefa:', err);
+        res.status(500).json({ error: 'Erro ao adicionar tarefa' });
     }
 });
 
